bf51e381065bcdfba97c95e13ff06429e941af6d
genesis331 <cheah3838@gmail.com>
2022-11-02 00:30:09 +0800

    update package name & try to build jar

--- src/main/java/oopassignment/actnow/Actnow.java
+++ src/main/java/oopassignment/actnow/Actnow.java
@@ -4,12 +4,31 @@
  */
 package oopassignment.actnow;
 
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 /**
  *
  * @author ZIXU
  */
 public class Actnow {
     public static void main (String[] args) {
-        new Welcome().setVisible(true);
+        Platform.startup(() -> {
+            FXMLLoader fxmlLoader = new FXMLLoader(WelcomeUI.class.getResource("welcome-view.fxml"));
+            Scene scene = null;
+            try {
+                scene = new Scene(fxmlLoader.load(), 550, 620);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Stage stage = new Stage();
+            stage.setTitle("Welcome to ActNow");
+            stage.setScene(scene);
+            stage.show();
+        });
     }
 }
