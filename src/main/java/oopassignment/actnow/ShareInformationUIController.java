53d7b606f6ea3d5757a728b45ad2e80bfb0e0e26
genesis331 <cheah3838@gmail.com>
2022-11-09 01:11:11 +0800

    add download functionality

--- src/main/java/oopassignment/actnow/ShareInformationUIController.java
+++ src/main/java/oopassignment/actnow/ShareInformationUIController.java
@@ -3,7 +3,9 @@ package oopassignment.actnow;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
@@ -16,11 +18,33 @@ public class ShareInformationUIController {
     private Label downloadLabel;
 
     @FXML
+    private Label hiddenLabel;
+
+    @FXML
     public void setContent(String filename) throws IOException {
         File targetFile = new File("guides/" + filename);
         Scanner scanner = new Scanner(targetFile);
         String title = scanner.nextLine().substring(2);
         String disaster = scanner.nextLine();
         downloadLabel.setText(disaster + " - " + title);
+        hiddenLabel.setText(filename);
+    }
+
+    @FXML
+    private void downloadBtnClick() throws IOException {
+        String filename = hiddenLabel.getText();
+        File targetFile = new File("guides/" + filename);
+        String content = new Scanner(targetFile).useDelimiter("\\Z").next();
+
+        File saveFile = new File("downloads/" + filename);
+        java.io.FileWriter writer = new java.io.FileWriter(saveFile);
+        writer.write(content);
+        writer.close();
+
+        JFrame f = new JFrame();
+        JOptionPane.showMessageDialog(f, "File downloaded successfully!");
+
+        Stage stage = (Stage) shareInformationPane.getScene().getWindow();
+        stage.close();
     }
 }
